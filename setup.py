04fe1ecc3c03349d184a5df80b68a867efa05649
TWEApol <tom142606@gmail.com>
2018-12-19 16:22:20 +0100

    again, attempt to link personal FMD source

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ setuptools.setup(
     description="API for Zeeguu, a project that aims to accelerate vocabulary acquisition in a second language",
     keywords=" API, second language acquisition",
     
-    install_requires= [
-        "my_flask_monitoringdashboard"
-    ],
     dependency_link = [
         "https://github.com/TWEApol/Flask-MonitoringDashboard/tarball/master#egg=my_flask_monitoringdashboard"
+    ],
+    install_requires= [
+        "my_flask_monitoringdashboard"
     ],
 )
