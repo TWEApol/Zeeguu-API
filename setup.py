144dfec92c211d96db4f5598a221ff19a4bea7eb
TWEApol <tom142606@gmail.com>
2018-12-19 15:56:48 +0100

    Added dependency to personal version of FMD

--- setup.py
+++ setup.py
@@ -13,4 +13,11 @@ setuptools.setup(
     author_email="zeeguu.team@gmail.com",
     description="API for Zeeguu, a project that aims to accelerate vocabulary acquisition in a second language",
     keywords=" API, second language acquisition",
+    
+    install_requires= [
+        "flask_monitoringdashboard"
+    ],
+    dependency_link = [
+        "https://github.com/TWEApol/Flask-MonitoringDashboard/tarball/master#egg=flask_monitoringdashboard"
+    ],
 )
